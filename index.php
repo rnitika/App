ace0f9f2e24c07c000f144bed9ffc073d2b342ec
rnitika <103918976+rnitika@users.noreply.github.com>
2022-07-15 17:13:08 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang ="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="description" contents="About my new hostel website">
+        <link rel="stylesheet" href="style.css" type="text/css">
+    
+    </head>
+    <body>
+    <img src="https://www.thebusinessplanshop.com/en/blog/how-to-open-a-hostel" alt="Simply Easy Learning" width="500" height="50">
+            <div class="bgimg">
+                <div class="center">
+                <p>WELCOME</p>
+            </div>
+            
+
+
+        <header>
+            <nav id="navigation">
+            <div class="Wrapper">
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    
+                    <li><a href="about.php">About</a></li>
+                    <li><a href="login.php">Login</a></li>
+                   
+                    
+                </ul>
+            </div>
+            </nav>
+        </header>
+    </body>
+</html>
